a32fdc2f378a9deaa60887f13bced06490f7a368
Song nari <na061300@naver.com>
2021-12-19 01:00:50 +0900

    Update Title.cs
    씬 이동 추가

--- Assets/Scripts/Title/Title.cs
+++ Assets/Scripts/Title/Title.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class Title : MonoBehaviour
                 timer += Time.deltaTime;
             Debug.Log("STARG GAME");
             //æ¿ ¿Ãµø
+            SceneManager.LoadScene("SampleScene");
         }
     }
 
